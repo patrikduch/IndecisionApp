edce0b403f4e490ac6e0884ac96b8657add72b38
patrikduch <duchpatrik@icloud.com>
2020-10-01 04:30:31 +0200

    Server CSS stylesheet fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const publicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
